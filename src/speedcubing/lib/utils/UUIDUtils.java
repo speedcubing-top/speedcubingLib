c18c5e4bf6d761ee3fbfb5251cfe43400493e1ae
speedcubing <speedcubingthebest@gmail.com>
2022-05-02 10:35:04 +0800

    add uuid check

--- src/speedcubing/lib/utils/UUIDUtils.java
+++ src/speedcubing/lib/utils/UUIDUtils.java
@@ -5,7 +5,7 @@ import java.util.UUID;
 public class UUIDUtils {
 
     public static String addDash(String uuid) {
-        return uuid.substring(0, 8) + "-" + uuid.substring(8, 12) + "-" + uuid.substring(12, 16) + "-" + uuid.substring(16, 20) + "-" + uuid.substring(20);
+        return uuid.length() == 38 ? uuid : uuid.substring(0, 8) + "-" + uuid.substring(8, 12) + "-" + uuid.substring(12, 16) + "-" + uuid.substring(16, 20) + "-" + uuid.substring(20);
     }
 
     public static String removeDash(String uuid) {
