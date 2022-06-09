6af3399deeaab1b4d93172dcbf93e07b578331c5
BuildTools <unconfigured@null.spigotmc.org>
2022-06-09 16:25:06 +0800

    unicode

---
+++ src/speedcubing/lib/utils/StringUtils.java
@@ -0,0 +1,20 @@
+package speedcubing.lib.utils;
+
+public class StringUtils {
+    public static String encodeUnicode(String s) {
+        StringBuilder unicode = new StringBuilder();
+        for (int i = 0; i < s.length(); i++) {
+            unicode.append("\\u").append(Integer.toHexString(s.charAt(i)));
+        }
+        return unicode.toString();
+    }
+
+    public static String decodeUnicode(String unicode) {
+        StringBuilder text = new StringBuilder("§8[§cTerminal§8] §f");
+        String[] hex = unicode.split("\\\\u");
+        for (int i = 1; i < hex.length; i++) {
+            text.append((char) Integer.parseInt(hex[i], 16));
+        }
+        return text.toString();
+    }
+}
