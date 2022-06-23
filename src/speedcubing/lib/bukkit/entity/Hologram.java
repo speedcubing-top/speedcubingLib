7e0bd30fdc9f766fb41df96eb7a7151df96a228e
BuildTools <unconfigured@null.spigotmc.org>
2022-06-23 16:56:41 +0800

    hologram & npc api

---
+++ src/speedcubing/lib/bukkit/entity/Hologram.java
@@ -0,0 +1,44 @@
+package speedcubing.lib.bukkit.entity;
+
+import net.minecraft.server.v1_8_R3.*;
+import org.bukkit.Bukkit;
+import org.bukkit.craftbukkit.v1_8_R3.CraftWorld;
+
+import java.util.Set;
+
+public class Hologram {
+    public Set<PlayerConnection> listener;
+    public EntityArmorStand armorStand;
+
+    public Hologram(String name, double x, double y, double z, float yaw, float pitch) {
+        armorStand = new EntityArmorStand(((CraftWorld) Bukkit.getWorld("world")).getHandle());
+        armorStand.setCustomNameVisible(true);
+        armorStand.setGravity(true);
+        armorStand.setInvisible(true);
+        armorStand.n(true);
+        armorStand.setCustomName(name);
+        armorStand.setLocation(x, y, z, yaw, pitch);
+    }
+
+    public Hologram spawn() {
+        PacketPlayOutSpawnEntityLiving packet = new PacketPlayOutSpawnEntityLiving(armorStand);
+        listener.forEach(a -> a.sendPacket(packet));
+        return this;
+    }
+
+    public Hologram despawn() {
+        PacketPlayOutEntityDestroy packet = new PacketPlayOutEntityDestroy(armorStand.getId());
+        listener.forEach(a -> a.sendPacket(packet));
+        return this;
+    }
+
+
+    public Hologram setName(String name, boolean update) {
+        armorStand.setCustomName(name);
+        if (update) {
+            PacketPlayOutEntityMetadata packet = new PacketPlayOutEntityMetadata(armorStand.getId(), armorStand.getDataWatcher(), true);
+            listener.forEach(a -> a.sendPacket(packet));
+        }
+        return this;
+    }
+}
---
+++ src/speedcubing/lib/bukkit/entity/NPC.java
@@ -0,0 +1,99 @@
+package speedcubing.lib.bukkit.entity;
+
+import com.mojang.authlib.GameProfile;
+import com.mojang.authlib.properties.Property;
+import net.minecraft.server.v1_8_R3.*;
+import org.bukkit.Bukkit;
+import org.bukkit.craftbukkit.v1_8_R3.CraftServer;
+import org.bukkit.craftbukkit.v1_8_R3.CraftWorld;
+import org.bukkit.craftbukkit.v1_8_R3.inventory.CraftItemStack;
+import org.bukkit.inventory.ItemStack;
+import speedcubing.lib.utils.Reflections;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.UUID;
+
+public class NPC {
+    public final EntityPlayer entityPlayer;
+    public final int id;
+    public Set<PlayerConnection> listener = new HashSet<>();
+
+    public NPC(String name, UUID uuid, boolean outerLayer) {
+        WorldServer world = ((CraftWorld) Bukkit.getWorld("world")).getHandle();
+        this.entityPlayer = new EntityPlayer(((CraftServer) Bukkit.getServer()).getServer(), world, new GameProfile(uuid, name), new PlayerInteractManager(world));
+        this.id = entityPlayer.getId();
+        if (outerLayer)
+            entityPlayer.getDataWatcher().watch(10, (byte) 127);
+    }
+
+    public NPC animation(int animation) {
+        PacketPlayOutAnimation packet = new PacketPlayOutAnimation();
+        Reflections.setField(packet, "a", id);
+        Reflections.setField(packet, "b", (byte) animation);
+        listener.forEach(a -> a.sendPacket(packet));
+        return this;
+    }
+
+    public NPC status(int status) {
+        PacketPlayOutEntityStatus packet = new PacketPlayOutEntityStatus();
+        Reflections.setField(packet, "a", id);
+        Reflections.setField(packet, "b", (byte) status);
+        listener.forEach(a -> a.sendPacket(packet));
+        return this;
+    }
+
+    public NPC items(ItemStack stack) {
+        PacketPlayOutEntityEquipment packet = new PacketPlayOutEntityEquipment(id, 0, CraftItemStack.asNMSCopy(stack));
+        listener.forEach(a -> a.sendPacket(packet));
+        return this;
+    }
+
+    public NPC spawn(float yaw) {
+        PacketPlayOutPlayerInfo p1 = new PacketPlayOutPlayerInfo(PacketPlayOutPlayerInfo.EnumPlayerInfoAction.ADD_PLAYER, entityPlayer);
+        PacketPlayOutNamedEntitySpawn p2 = new PacketPlayOutNamedEntitySpawn(entityPlayer);
+        PacketPlayOutEntityHeadRotation p3 = new PacketPlayOutEntityHeadRotation(entityPlayer, (byte) ((int) (yaw * 256 / 360)));
+        listener.forEach(a -> {
+                    a.sendPacket(p1);
+                    a.sendPacket(p2);
+                    a.sendPacket(p3);
+                }
+        );
+        return this;
+    }
+
+    public NPC setSkin(String value, String signature, boolean update) {
+        GameProfile gameProfile = entityPlayer.getProfile();
+        gameProfile.getProperties().removeAll("textures");
+        gameProfile.getProperties().put("textures", new Property("textures", value, signature));
+        if (update) {
+            despawn();
+            spawn(entityPlayer.yaw);
+            updateNpcLocation();
+        }
+        return this;
+    }
+
+    public NPC despawn() {
+        PacketPlayOutEntityDestroy packet = new PacketPlayOutEntityDestroy(id);
+        listener.forEach(a -> a.sendPacket(packet));
+        return this;
+    }
+
+    public NPC setLocation(double x, double y, double z, float yaw, float pitch) {
+        entityPlayer.setLocation(x, y, z, yaw, pitch);
+        return this;
+    }
+    public NPC updateNpcLocation() {
+        PacketPlayOutEntityTeleport p1 = new PacketPlayOutEntityTeleport(id, MathHelper.floor(entityPlayer.locX * 32), MathHelper.floor(entityPlayer.locY * 32), MathHelper.floor(entityPlayer.locZ * 32), (byte) ((int) (entityPlayer.yaw * 256 / 360)), (byte) ((int) (entityPlayer.pitch * 256 / 360)), true);
+        PacketPlayOutEntity.PacketPlayOutEntityLook p2 = new PacketPlayOutEntity.PacketPlayOutEntityLook(id, (byte) ((int) (entityPlayer.yaw * 256F / 360F)), (byte) ((int) (entityPlayer.pitch * 256F / 360F)), true);
+        PacketPlayOutEntityHeadRotation p3 = new PacketPlayOutEntityHeadRotation(entityPlayer, (byte) ((int) (entityPlayer.yaw * 256F / 360F)));
+        listener.forEach(a -> {
+                    a.sendPacket(p1);
+                    a.sendPacket(p2);
+                    a.sendPacket(p3);
+                }
+        );
+        return this;
+    }
+}
