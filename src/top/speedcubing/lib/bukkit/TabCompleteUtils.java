0a8986df197e14c4d775e741809dba4c6a088c7c
speedcubing <speedcubingthebest@gmail.com>
2022-09-17 00:02:13 +0800

    utilities

---
+++ src/top/speedcubing/lib/bukkit/TabCompleteUtils.java
@@ -0,0 +1,19 @@
+package top.speedcubing.lib.bukkit;
+
+import org.bukkit.Bukkit;
+import org.bukkit.command.PluginCommand;
+import org.bukkit.command.TabCompleter;
+
+import java.util.Collections;
+
+public class TabCompleteUtils {
+    static TabCompleter completer = (commandSender, command, s1, strings) -> Collections.emptyList();
+
+    public static void registerEmptyTabComplete(String... commands) {
+        for (String s : commands) {
+            PluginCommand command = Bukkit.getPluginCommand(s);
+            if (command != null)
+                command.setTabCompleter(completer);
+        }
+    }
+}
--- src/top/speedcubing/lib/speedcubingLibBukkit.java
+++ src/top/speedcubing/lib/speedcubingLibBukkit.java
@@ -7,14 +7,15 @@ import top.speedcubing.lib.bukkit.inventory.Glow;
 import top.speedcubing.lib.bukkit.listeners.PacketListener;
 import top.speedcubing.lib.bukkit.listeners.PlayerListener;
 
+import java.io.File;
 import java.lang.reflect.Field;
 
 public class speedcubingLibBukkit extends JavaPlugin {
-    public static boolean is1_8_8;
+    public static boolean deletePlayerFile = false;
+    public static final boolean is1_8_8 = Bukkit.getVersion().contains("(MC: 1.8.8)");
 
     public void onEnable() {
         Bukkit.getMessenger().registerOutgoingPluginChannel(this, "BungeeCord");
-        is1_8_8 = Bukkit.getVersion().contains("(MC: 1.8.8)");
         Bukkit.getPluginManager().registerEvents(new PacketListener(), this);
         Bukkit.getPluginManager().registerEvents(new PlayerListener(), this);
         if (is1_8_8)
@@ -26,5 +27,20 @@ public class speedcubingLibBukkit extends JavaPlugin {
                 Enchantment.registerEnchantment(Glow.glow);
             } catch (Exception exception) {
             }
+    }
+
+    public void onDisable() {
+        if (deletePlayerFile) {
+            File index = new File(Bukkit.getWorld("world").getWorldFolder() + "/playerdata");
+            File index2 = new File(Bukkit.getWorld("world").getWorldFolder() + "/stats");
+            if (index.list() != null)
+                for (String s : index.list())
+                    new File(index.getPath(), s).delete();
+            if (index2.list() != null)
+                for (String s : index2.list())
+                    new File(index2.getPath(), s).delete();
+            index.delete();
+            index2.delete();
+        }
     }
 }
