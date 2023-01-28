b663caae07394e6c0f1b543417755854b8188c95
speedcubing <speedcubingthebest@gmail.com>
2023-01-29 03:59:56 +0800

    make bungee textbuilder works in spigot aswell

--- src/top/speedcubing/lib/bungee/TextBuilder.java
+++ src/top/speedcubing/lib/bungee/TextBuilder.java
@@ -1,48 +1,56 @@
 package top.speedcubing.lib.bungee;
 
 
-import net.md_5.bungee.api.chat.*;
+import net.md_5.bungee.api.chat.BaseComponent;
+import net.md_5.bungee.api.chat.ClickEvent;
+import net.md_5.bungee.api.chat.HoverEvent;
+import net.md_5.bungee.api.chat.TextComponent;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class TextBuilder {
-    private final ComponentBuilder builder = new ComponentBuilder();
-
-    public TextBuilder() {
-    }
+    private final List<BaseComponent> builder = new ArrayList<>();
 
     public TextBuilder str(String s) {
-        this.builder.append(TextComponent.fromLegacyText(s));
+        this.builder.addAll(Arrays.asList(TextComponent.fromLegacyText(s)));
         return this;
     }
 
-    public TextBuilder click(String s, ClickEvent c) {
+    public TextBuilder click(String s, ClickEvent.Action clickevent, String clickeventstring) {
         BaseComponent[] b = TextComponent.fromLegacyText(s);
+        ClickEvent c = new ClickEvent(clickevent, clickeventstring);
         for (BaseComponent a : b) {
             a.setClickEvent(c);
+            builder.add(a);
         }
-        this.builder.append(b);
         return this;
     }
 
-    public TextBuilder hover(String s, HoverEvent h) {
+    public TextBuilder hover(String s, HoverEvent.Action hoverevent, BaseComponent[] hovereventstring) {
         BaseComponent[] b = TextComponent.fromLegacyText(s);
+        HoverEvent h = new HoverEvent(hoverevent, hovereventstring);
         for (BaseComponent a : b) {
             a.setHoverEvent(h);
+            builder.add(a);
         }
-        this.builder.append(b);
         return this;
     }
 
-    public TextBuilder both(String s, ClickEvent c, HoverEvent h) {
+    public TextBuilder both(String s, ClickEvent.Action clickevent, String clickeventstring, HoverEvent.Action hoverevent, BaseComponent[] hovereventstring) {
         BaseComponent[] b = TextComponent.fromLegacyText(s);
+        ClickEvent c = new ClickEvent(clickevent, clickeventstring);
+        HoverEvent h = new HoverEvent(hoverevent, hovereventstring);
         for (BaseComponent a : b) {
-            a.setHoverEvent(h);
             a.setClickEvent(c);
+            a.setHoverEvent(h);
+            builder.add(a);
         }
-        this.builder.append(b);
         return this;
     }
 
     public BaseComponent[] build() {
-        return this.builder.create();
+        return builder.toArray(new BaseComponent[0]);
     }
 }
