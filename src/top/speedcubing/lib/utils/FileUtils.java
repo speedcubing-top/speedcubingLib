0edf24d0d257605021fdd93cd1304093ad9bb9a5
BuildTools <unconfigured@null.spigotmc.org>
2023-04-26 14:46:26 +0800

    add util

--- src/top/speedcubing/lib/utils/FileUtils.java
+++ src/top/speedcubing/lib/utils/FileUtils.java
@@ -1,21 +1,24 @@
 package top.speedcubing.lib.utils;
 
-import net.minecraft.server.v1_8_R3.Entity;
-import net.minecraft.server.v1_8_R3.MinecraftServer;
-import net.minecraft.server.v1_8_R3.WorldServer;
-
 import java.io.File;
-import java.util.UUID;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 
 public class FileUtils {
-    public static String getExtension(String s) {
-        int index = s.lastIndexOf(46);
-        index = Math.max(s.lastIndexOf(47), s.lastIndexOf(92)) > index ? -1 : index;
-        return index == -1 ? "" : s.substring(index + 1);
+    public static String getExtension(String f) {
+        int index = f.lastIndexOf(46);
+        index = Math.max(f.lastIndexOf(47), f.lastIndexOf(92)) > index ? -1 : index;
+        return index == -1 ? "" : f.substring(index + 1);
     }
 
-    public static void deleteDirectory(File directory) {
-        for (File file : directory.listFiles()) {
+    public static void deleteDirectory(String dir) {
+        deleteDirectory(new File(dir));
+    }
+
+    public static void deleteDirectory(File dir) {
+        for (File file : dir.listFiles()) {
             try {
                 if (file.isDirectory())
                     deleteDirectory(file);
@@ -25,6 +28,21 @@ public class FileUtils {
                 e.printStackTrace();
             }
         }
-        directory.delete();
+        dir.delete();
+    }
+
+    public static void copyDirectory(String fromDir, String toDir) {
+        Path source = Paths.get(fromDir);
+        try {
+            Files.walk(source).forEach(p -> {
+                try {
+                    Files.copy(p, Paths.get(toDir).resolve(source.relativize(p)), StandardCopyOption.REPLACE_EXISTING);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            });
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
