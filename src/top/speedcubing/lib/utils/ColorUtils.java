33b2989b8719801e969ff17d9ac6fa3865796451
BuildTools <unconfigured@null.spigotmc.org>
2023-08-26 18:34:18 +0800

    add things

--- src/top/speedcubing/lib/utils/ColorUtils.java
+++ src/top/speedcubing/lib/utils/ColorUtils.java
@@ -13,4 +13,12 @@ public class ColorUtils {
     public static String toHex(Color c) {
         return String.format("%02X%02X%02X", c.getRed(), c.getGreen(), c.getBlue());
     }
+
+    public static int[] rgbToColor(int rgb) {
+        return new int[]{rgb >> 16 & 255, rgb >> 8 & 255, rgb & 255, rgb >> 24 & 255};
+    }
+
+    public static int colorToRGB(int r, int g, int b, int a) {
+        return ((a & 0xFF) << 24) | ((r & 0xFF) << 16) | ((g & 0xFF) << 8) | ((b & 0xFF));
+    }
 }
