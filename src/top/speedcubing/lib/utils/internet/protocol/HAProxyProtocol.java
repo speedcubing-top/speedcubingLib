91f8a88db67fddcf4f1befb934425240a5990983
speedcubing <speedcubingthebest@gmail.com>
2023-10-07 00:07:49 +0800

    proxy-protocol v1 support ipv6

--- src/top/speedcubing/lib/utils/internet/protocol/HAProxyProtocol.java
+++ src/top/speedcubing/lib/utils/internet/protocol/HAProxyProtocol.java
@@ -2,13 +2,21 @@ package top.speedcubing.lib.utils.internet.protocol;
 
 import top.speedcubing.lib.utils.ByteArrayDataBuilder;
 
+import java.net.StandardProtocolFamily;
+
 public class HAProxyProtocol {
-    public static byte[] v1(String sourceIPV4, String destIPV4, int sourcePort, int destPort) {
+
+    public static byte[] v1(StandardProtocolFamily protocolFamily, String sourceAddr, String destAddr, int sourcePort, int destPort) {
         ByteArrayDataBuilder builder = new ByteArrayDataBuilder();
-        builder.write(new byte[]{80, 82, 79, 88, 89, 32, 84, 67, 80, 52, 32});
-        builder.write(sourceIPV4.getBytes());
+        builder.write(new byte[]{80, 82, 79, 88, 89, 32, 84, 67, 80});
+        if (protocolFamily == StandardProtocolFamily.INET)
+            builder.write(52);
+        else if (protocolFamily == StandardProtocolFamily.INET6)
+            builder.write(54);
         builder.write(32);
-        builder.write(destIPV4.getBytes());
+        builder.write(sourceAddr.getBytes());
+        builder.write(32);
+        builder.write(destAddr.getBytes());
         builder.write(32);
         builder.write(String.valueOf(sourcePort).getBytes());
         builder.write(32);
