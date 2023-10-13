c4811d0ba8d4228d19b30943b687821f1ba71c5a
speedcubing <speedcubingthebest@gmail.com>
2023-10-13 12:48:15 +0800

    update

---
+++ src/top/speedcubing/lib/utils/bytes/NumberConversion.java
@@ -0,0 +1,60 @@
+package top.speedcubing.lib.utils.bytes;
+
+public class NumberConversion {
+    public static String toBinary(int i) {
+        return toBinary(i, 0xFFFFFF);
+    }
+
+    public static String toBinary(short i) {
+        return toBinary(i, 0xFFFF);
+    }
+
+
+    public static String toBinary(byte i) {
+        return toBinary(i, 0xFF);
+    }
+
+    public static String toBinary(char i) {
+        return toBinary(i, 0xFF);
+    }
+
+    public static String toBinary(int i, int range) {
+        return Integer.toBinaryString(i & range);
+    }
+
+    public static String toHex(int i) {
+        return toHex(i, 0xFFFFFFFF);
+    }
+
+    public static String toHex(short i) {
+        return toHex(i, 0xFFFF);
+    }
+
+    public static String toHex(byte i) {
+        return toHex(i, 0xFF);
+    }
+
+    public static String toHex(char i) {
+        return toHex(i, 0xFF);
+    }
+
+    public static String toHex(int i, int range) {
+        return Integer.toHexString(i & range).toUpperCase();
+    }
+
+    public static int toDecimal(String s, int radix) {
+        return Integer.parseInt(s, radix);
+    }
+
+    public static void main(String[] s){
+        System.out.println(toBinary(0xFF));
+    }
+
+    public static int toUnsigned(byte b) {
+        return ((int) b) & 0xFF;
+    }
+
+    public static long toUnsigned(int i) {
+        return ((long) i) & 0xFFFFFFFFL;
+    }
+}
