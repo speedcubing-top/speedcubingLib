f5a7eb5a0b21f30398e73f6caaabf84155da6dca
speedcubing <speedcubingthebest@gmail.com>
2024-01-01 18:47:27 +0800

    optimize sidebar

--- src/top/speedcubing/lib/bukkit/SideBar.java
+++ src/top/speedcubing/lib/bukkit/SideBar.java
@@ -3,10 +3,7 @@ package top.speedcubing.lib.bukkit;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.event.Listener;
-import org.bukkit.scoreboard.DisplaySlot;
-import org.bukkit.scoreboard.Objective;
-import org.bukkit.scoreboard.Scoreboard;
-import org.bukkit.scoreboard.Team;
+import org.bukkit.scoreboard.*;
 import top.speedcubing.lib.utils.collection.Sets;
 
 import java.util.*;
@@ -46,9 +43,9 @@ public class SideBar implements Listener {
         player.setScoreboard(scoreboard);
     }
 
-    SideBar setPrefixSuffix(String str, String team) {
+    private SideBar setPrefixSuffix(String str, String team) {
         Team t = scoreboard.getTeam(team);
-        String[] result = split(str);
+        String[] result = split(str, team.charAt(3));
         if (!result[0].equals(t.getPrefix()))
             t.setPrefix(result[0]);
         if (!result[1].equals(t.getSuffix()))
@@ -75,19 +72,15 @@ public class SideBar implements Listener {
         return this;
     }
 
-    public SideBar changeableLine(String prefix, String suffix, int score) {
-        changer += 1;
-        String str = "§" + "!0123456789abcdef".charAt(changer) + "§f";
-        Team x = scoreboard.registerNewTeam(str);
-        x.addEntry(str);
-        x.setPrefix(prefix);
-        x.setSuffix(suffix);
-        return add(str, score, false);
-    }
-
     public SideBar changeableLine(String str, int score) {
-        String[] result = split(str);
-        return changeableLine(result[0], result[1], score);
+        changer += 1;
+        String team = "§" + "!0123456789abcdef".charAt(changer) + "§f";
+        String[] result = split(str, team.charAt(3));
+        Team t = scoreboard.registerNewTeam(team);
+        t.addEntry(team);
+        t.setPrefix(result[0]);
+        t.setSuffix(result[1]);
+        return add(team, score, false);
     }
 
     public SideBar setLine(String str, int line) {
@@ -101,7 +94,7 @@ public class SideBar implements Listener {
         return this;
     }
 
-    private static String[] split(String str) {
+    private static String[] split(String str, char c) {
         String prefix;
         String suffix = "";
         if (str.length() <= 16)
@@ -110,19 +103,24 @@ public class SideBar implements Listener {
             boolean splitted = false;
             if (str.charAt(15) == '§') {
                 prefix = str.substring(0, 15);
-                if ((int) str.charAt(16) < 107)
+                if (str.charAt(16) < 'k')
                     suffix = str.substring(15);
             } else {
                 splitted = true;
                 prefix = str.substring(0, 16);
             }
             for (int j = splitted ? 14 : 13; j >= 0; j--) {
-                if (str.charAt(j) == '§' && (int) str.charAt(j + 1) < 107) {
-                    suffix = "§" + str.charAt(j + 1) + str.substring(splitted ? 16 : 15);
+                char next = str.charAt(j + 1);
+                if (str.charAt(j) == '§' && next < 'k') {
+                    suffix = "§" + next + str.substring(splitted ? 16 : 15);
                     break;
                 }
             }
-            suffix = suffix.equals("") ? str.substring(splitted ? 16 : 15) : suffix;
+            suffix = suffix.isEmpty() ? str.substring(splitted ? 16 : 15) : suffix;
+        }
+        if (suffix.length() > 1) {
+            if (suffix.charAt(0) == '§' && suffix.charAt(1) == c)
+                suffix = suffix.substring(2);
         }
         return new String[]{prefix, suffix};
     }
