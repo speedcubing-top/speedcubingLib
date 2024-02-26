2f48b3ce4a76d5cb14d652fa13d578006185fdca
speedcubing <speedcubingthebest@gmail.com>
2024-02-26 20:55:38 +0800

    added something

---
+++ src/top/speedcubing/lib/utils/CryptoUtils.java
@@ -0,0 +1,32 @@
+package top.speedcubing.lib.utils;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.security.DigestInputStream;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class CryptoUtils {
+    public static String toSHA256(File f) {
+        try {
+            MessageDigest digest = MessageDigest.getInstance("SHA-256");
+            try (DigestInputStream dis = new DigestInputStream(new FileInputStream(f), digest)) {
+                while (dis.read() != -1) {
+                }
+                char[] HEX_ARRAY = "0123456789ABCDEF".toCharArray();
+                byte[] hash = digest.digest();
+                char[] hexChars = new char[hash.length * 2];
+                for (int j = 0; j < hash.length; j++) {
+                    int v = hash[j] & 0xFF;
+                    hexChars[j * 2] = HEX_ARRAY[v >>> 4];
+                    hexChars[j * 2 + 1] = HEX_ARRAY[v & 0x0F];
+                }
+                return new String(hexChars);
+            }
+        } catch (NoSuchAlgorithmException | IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
