bfc91ebccb7d0b7fe77a68e9cf6a9ae4275121af
SeanMud0319 <whe91000@gmail.com>
2024-03-04 09:58:27 +0800

    Added Discord webhook utils

---
+++ src/main/java/top/speedcubing/lib/discord/SimpleWebhook.java
@@ -0,0 +1,20 @@
+package top.speedcubing.lib.discord;
+
+import java.awt.Color;
+import java.io.IOException;
+
+public class SimpleWebhook {
+    public static void sendWebhook(String webhookUrl, String title, String description, String imageUrl, Color color) {
+        DiscordWebhook discordWebhook = new DiscordWebhook(webhookUrl);
+        discordWebhook.addEmbed(new DiscordWebhook.EmbedObject()
+                .setTitle(title)
+                .setDescription(description)
+                .setImage(imageUrl)
+                .setColor(color));
+        try {
+            discordWebhook.execute();
+        } catch (IOException en) {
+            throw new RuntimeException(en);
+        }
+    }
+}
