c614c91f1ec260e7d9df01f3150ebe94db94964f
speedcubing <speedcubingthebest@gmail.com>
2024-10-02 18:08:18 +0800

    modified new PotionEffect

--- src/main/java/top/speedcubing/lib/bukkit/inventory/ItemBuilder.java
+++ src/main/java/top/speedcubing/lib/bukkit/inventory/ItemBuilder.java
@@ -9,13 +9,13 @@ import java.util.List;
 import java.util.UUID;
 import org.bukkit.Material;
 import org.bukkit.enchantments.Enchantment;
-import org.bukkit.inventory.AnvilInventory;
 import org.bukkit.inventory.ItemFlag;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.inventory.meta.PotionMeta;
 import org.bukkit.inventory.meta.SkullMeta;
 import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
 import top.speedcubing.lib.utils.ReflectionUtils;
 
 public class ItemBuilder {
@@ -33,18 +33,37 @@ public class ItemBuilder {
         meta = itemStack.getItemMeta();
     }
 
-    public ItemBuilder durability(int s) {
-        itemStack.setDurability((short) s);
-        return this;
-    }
-
     public ItemBuilder amount(int amount) {
         itemStack.setAmount(amount);
         return this;
     }
 
+    public ItemBuilder durability(int s) {
+        itemStack.setDurability((short) s);
+        return this;
+    }
+
+    public ItemBuilder ench(Enchantment enchantment, int level) {
+        meta.addEnchant(enchantment, level, true);
+        return this;
+    }
+
     public ItemBuilder name(String displayName) {
         meta.setDisplayName(displayName);
+        return this;
+    }
+
+    public ItemBuilder owner(String name) {
+        SkullMeta skull = (SkullMeta) meta;
+        skull.setOwner(name);
+        itemStack.setItemMeta(skull);
+        return this;
+    }
+
+    public ItemBuilder potion(PotionEffectType type, int duration, int amplifier) {
+        PotionMeta potion = (PotionMeta) meta;
+        potion.addCustomEffect(new PotionEffect(type, duration, amplifier), true);
+        itemStack.setItemMeta(potion);
         return this;
     }
 
@@ -54,23 +73,28 @@ public class ItemBuilder {
     }
 
     public ItemBuilder lore(String... lore) {
-        meta.setLore(Arrays.asList(lore));
+        lore(Arrays.asList(lore));
+        return this;
+    }
+
+    public ItemBuilder lore(List<String> lore) {
+        meta.setLore(lore);
         return this;
     }
 
     public ItemBuilder addLore(String... lore) {
-        List<String> s = meta.getLore();
-        if (s == null) {
-            meta.setLore(Arrays.asList(lore));
-        } else {
-            s.addAll(Arrays.asList(lore));
-            meta.setLore(s);
-        }
+        addLore(Arrays.asList(lore));
         return this;
     }
 
-    public ItemBuilder hideUnBreak() {
-        meta.addItemFlags(ItemFlag.HIDE_UNBREAKABLE);
+    public ItemBuilder addLore(List<String> lore) {
+        List<String> s = meta.getLore();
+        if (s == null) {
+            lore(lore);
+        } else {
+            s.addAll(lore);
+            meta.setLore(s);
+        }
         return this;
     }
 
@@ -89,28 +113,13 @@ public class ItemBuilder {
         return this;
     }
 
+    public ItemBuilder hideUnbreak() {
+        meta.addItemFlags(ItemFlag.HIDE_UNBREAKABLE);
+        return this;
+    }
+
     public ItemBuilder glow() {
         meta.addEnchant(Glow.glow, 1, true);
-        return this;
-    }
-
-    @Deprecated
-    public ItemBuilder ench(Enchantment[] enchantments, int[] levels) {
-        for (int i = 0; i < enchantments.length; i++) {
-            meta.addEnchant(enchantments[i], levels[i], true);
-        }
-        return this;
-    }
-
-    public ItemBuilder ench(Enchantment enchantment, int level) {
-        meta.addEnchant(enchantment, level, true);
-        return this;
-    }
-
-    public ItemBuilder owner(String name) {
-        SkullMeta skull = (SkullMeta) meta;
-        skull.setOwner(name);
-        itemStack.setItemMeta(skull);
         return this;
     }
 
@@ -135,16 +144,9 @@ public class ItemBuilder {
     public ItemBuilder skullFromProfileValue(String profileValueBase64) {
         return skullFromURL(
                 JsonParser.parseString(new String(Base64.getDecoder().decode(profileValueBase64))).getAsJsonObject()
-                .getAsJsonObject("textures")
+                        .getAsJsonObject("textures")
                         .getAsJsonObject("SKIN")
                         .get("url").getAsString());
-    }
-
-    public ItemBuilder addPotion(PotionEffect effect) {
-        PotionMeta potion = (PotionMeta) meta;
-        potion.addCustomEffect(effect, true);
-        itemStack.setItemMeta(potion);
-        return this;
     }
 
     public ItemStack build() {
